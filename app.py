27d002835a8558a7a28dfee2cf82b3db62416cb4
abhi1kr2 <abhi1kr2@gmail.com>
2026-03-03 20:41:07 +0530

    Adding Student Login Frontend Code

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+#Calling the Student login form
+@app.route("/student_login")
+def student_login():
+    return render_template("student_login.html")
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
