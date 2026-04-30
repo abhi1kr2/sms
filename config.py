36a3d7ce66cef69393ccc8be6e18c7ea9b093fe5
Your Name <your_email@example.com>
2026-04-30 20:25:02 +0530

    Due to MySQL v8- need to install cryptography so added in requiremets

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-
 import os
+
 class Config:
     SECRET_KEY = "mysecret123"
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "abhi")   # your local password
-DB_NAME = os.getenv("DB_NAME", "rkdf_pro")
+
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "abhi")
+    DB_NAME = os.getenv("DB_NAME", "rkdf_pro")
+
+# expose for db.py
+DB_HOST = Config.DB_HOST
+DB_USER = Config.DB_USER
+DB_PASSWORD = Config.DB_PASSWORD
+DB_NAME = Config.DB_NAME
--- db.py
+++ db.py
@@ -1,11 +1,17 @@
 import pymysql
+import time
 from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 def get_db():
-    return pymysql.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME,
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    for i in range(5):  # retry 5 times
+        try:
+            return pymysql.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                database=DB_NAME,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except:
+            time.sleep(2)
+    raise Exception("Database not ready")
